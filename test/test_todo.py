a703eebd2e611507c838fa7427c2d5f4855cbc58
toms74209200 <motomotomato@gmail.com>
2024-03-10 14:39:02 +0900

    :sparkles: Add add todo

--- test/test_todo.py
+++ test/test_todo.py
@@ -1,7 +1,16 @@
 import requests
 
 
-def test_index():
-    response = requests.get("http://localhost:3000")
+def test_add_todo():
+    json = {
+        "name": "Buy milk",
+        "description": "Go to the store and buy milk",
+        "deadline": "2021-12-31T23:59:59.999Z",
+    }
+    response = requests.post("http://localhost:3000", json=json)
     assert response.status_code == 200
-    assert response.json() == []
+
+    response_index = requests.get("http://localhost:3000")
+    assert response_index.json()[0]["name"] == "Buy milk"
+    assert response_index.json()[0]["description"] == "Go to the store and buy milk"
+    assert response_index.json()[0]["deadline"] == "2021-12-31T23:59:59.999Z"
--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,14 +1,15 @@
-use axum::response::Json;
 use axum::{
+    extract::State,
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::get,
-    Router,
+    Json, Router,
 };
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
 use thiserror::Error;
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone)]
 struct Task {
     id: u32,
     name: String,
@@ -17,9 +18,35 @@ struct Task {
     completed: bool,
 }
 
-async fn index() -> Result<impl IntoResponse, MyError> {
-    let tasks: Vec<Task> = vec![];
-    Ok(Json(tasks))
+async fn index(State(tasks): State<Arc<Mutex<Vec<Task>>>>) -> Result<impl IntoResponse, MyError> {
+    Ok(Json(tasks.lock().unwrap().to_vec()))
+}
+
+#[derive(Deserialize)]
+struct AddRequest {
+    name: String,
+    description: String,
+    deadline: String,
+}
+
+#[derive(Serialize)]
+struct AddResponse {
+    id: u32,
+}
+
+async fn add_post(
+    State(tasks): State<Arc<Mutex<Vec<Task>>>>,
+    Json(req): Json<AddRequest>,
+) -> Result<impl IntoResponse, MyError> {
+    let id = tasks.lock().unwrap().len() as u32;
+    tasks.lock().unwrap().push(Task {
+        id,
+        name: req.name,
+        description: req.description,
+        deadline: req.deadline,
+        completed: false,
+    });
+    Ok(Json(AddResponse { id }))
 }
 
 #[derive(Error, Debug)]
@@ -33,7 +60,11 @@ impl IntoResponse for MyError {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(index));
+    let tasks: Arc<Mutex<Vec<Task>>> = Arc::new(Mutex::new(vec![]));
+
+    let app = Router::new()
+        .route("/", get(index).post(add_post))
+        .with_state(tasks);
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
